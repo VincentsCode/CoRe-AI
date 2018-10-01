8ade524b8279d3b6eb911622f18485f23fe0a770
VincentsCode <vincent.schmandt@gmx.de>
2018-10-01 23:59:52 +0200

    Using new API-Wrapper

--- ai1.py
+++ ai1.py
@@ -1,18 +1,27 @@
-import requests
 import time
-import json
-from Functions import play, free, get_symbol
 
+from BOAIapi import BOAIapi, MatchMethod
 
-username = "VincentsCode"
-password = "vincOmOBIL89*"
+def transpose(array):
+    array = array[:]  # make copy to avoid changing original
+    n = len(array)
+    for i, row in enumerate(array):
+        array[i] = row + [None for _ in range(n - len(row))]
+
+    array = zip(*array)
+
+    for i, row in enumerate(array):
+        array[i] = [elem for elem in row if elem is not None]
+
+    return array
 
 def turn(board, symbol):
-    # Dummy Player
-    
-    for i in range(8):
-        for j in range(8):
-            if free(i, j, board):
-                return i, j
+    board = transpose(board)
+    for x in range(8):
+        for y in range(8):
+            if board[x][y] == "#":
+                return x, y
 
-if __name__ == "__main__": play(username, password, turn)
+api = BOAIapi(turn, verbose=1, username="VincentsCode", password="vincOmOBIL89*")
+
+api.match(17, mode=MatchMethod.JOIN)
